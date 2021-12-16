65c08254c4ba8e816ea7a66f1c19ac24bf0daafb
EC2 Default User <ec2-user@ip-172-31-95-159.ec2.internal>
2021-12-16 15:32:02 +0000

    added files

--- LambdaDemo12-16-21/PythonSDKCodes/list_enesai_volumes.py
+++ LambdaDemo12-16-21/PythonSDKCodes/list_enesai_volumes.py
@@ -1,14 +1,13 @@
 import boto3
 
-# volume = boto3.client(service_name='ec2', region_name='us-east-1')
-
 ec2_console = boto3.session.Session(profile_name='DemoLambdaBoto')
-
 volume = ec2_console.client(service_name='ec2', region_name='us-east-1')
 
+# ============ Filtering ==================================
 list_of_volumes=[]
 enesai_volume_ids = {'Name':'tag:Name', 'Values':['enesai']}
-    
+
+#============== List enesai volumes ==============================================   
 for each_volume in volume.describe_volumes(Filters=[enesai_volume_ids])['Volumes']:
     list_of_volumes.append(each_volume['VolumeId'])
 print("The enesai volume ids are:  ",list_of_volumes)
